ea0f132891853e8112fed9867c4ebbf503ddc398
SassySerf <poorejustyn@gmail.com>
2025-03-19 20:02:04 -0600

    added letter count to stats.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import word_count
+from stats import letter_count
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -12,9 +13,12 @@ file_words = []
 file_words = file_contents.split()
 num_words = 0
 num_words = word_count(file_words)
+letter_count = letter_count(file_contents)
 
 def main():
 
     print(f"{num_words} words found in the document")
+    print(letter_count)
+
 
 main()
--- stats.py
+++ stats.py
@@ -3,3 +3,20 @@ def word_count(file_words):
     for word in file_words:
         count +=1
     return count
+
+def letter_count(fc):
+    fc.split()
+    letters = []
+    for word in fc:
+        word.split()
+        letters.append(word)
+    letter_count = {}
+    for i in letters:
+        i = i.lower()
+        dict_check = i in letter_count
+
+        if dict_check == False:
+            letter_count.setdefault(i, 1)
+        else:
+            letter_count[i] += 1
+    return letter_count
